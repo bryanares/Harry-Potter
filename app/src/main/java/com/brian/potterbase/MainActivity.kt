adbbadc6f8f1bc540daa520f0bfa211fa380240a
bryanares <bryanares12@gmail.com>
2022-10-13 17:11:10 +0300

    initial commit

--- app/src/main/java/com/brian/potterbase/MainActivity.kt
+++ app/src/main/java/com/brian/potterbase/MainActivity.kt
@@ -2,10 +2,15 @@ package com.brian.potterbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.brian.potterbase.ui.PotterListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
     }
+
 }
